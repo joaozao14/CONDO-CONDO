af1712d54f8b4df260793aec0bcfed5378bdc7b5
Guilherme Ribeiro <guilhermesp.ribeiro2004@gmail.com>
2026-07-22 15:20:07 -0300

    resolvI o conflito

--- java.java
+++ java.java
@@ -1,14 +1,9 @@
 public class java {
     
     public static void main(String[] args) {
-<<<<<<< HEAD
-        
-        String ronald_gay = "gay";
 
-=======
-        feijão com farinha;
-        receba, neithan bumbum guloso;
->>>>>>> 0ffffe6f883839af31be67a698c671f5dfbcd6d7
+        System.out.println("CONDO&CONDO");
+
     }
 
 }
