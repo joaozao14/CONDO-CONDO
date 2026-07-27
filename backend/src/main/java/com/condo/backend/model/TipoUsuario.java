ba9de43ba52b8daf81358b4dfedc811a97d2d298
Guilherme Ribeiro <guilhermesp.ribeiro2004@gmail.com>
2026-07-27 11:21:42 -0300

    classe usuario e tipo de usuarios

---
+++ backend/src/main/java/com/condo/backend/model/TipoUsuario.java
@@ -0,0 +1,10 @@
+package com.condo.backend.model;
+
+public enum TipoUsuario {
+
+    MORADOR,
+    PORTEIRO,
+    SINDICO,
+    ADMINISTRADOR
+
+}
---
+++ backend/src/main/java/com/condo/backend/model/Usuario.java
@@ -0,0 +1,12 @@
+package com.condo.backend.model;
+
+public class Usuario {
+
+   private String nome;
+   private String email;
+   private String senha;
+   private TipoUsuario TipoUsuario;
+
+
+
+}
